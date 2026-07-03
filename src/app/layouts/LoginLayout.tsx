9ddb30ca4db33979b68a77a262480451e57a1531
juuhye <165981565+juuhye@users.noreply.github.com>
2026-07-03 19:45:05 +0900

    refactor(router): coderabbit 수정

--- src/app/layouts/LoginLayout.tsx
+++ src/app/layouts/LoginLayout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { LoginPage } from '@/pages/login'
-import LoginBg from '@/shared/assets/login-bg.jpg'
 
 export const metadata: Metadata = {
   title: 'colorverse',
