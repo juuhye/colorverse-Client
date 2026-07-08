1c4415a196a9c9b5c53e1d2f07e02e0f8b5461fb
juuhye <165981565+juuhye@users.noreply.github.com>
2026-07-08 17:01:33 +0900

    feat(login-page): 'use client' 추가

--- src/widgets/login-card/ui/LoginFormCard.tsx
+++ src/widgets/login-card/ui/LoginFormCard.tsx
@@ -9,6 +9,7 @@ export function LoginFormCard() {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
       <div className='flex w-[53.5rem] flex-col gap-[4rem] rounded-28 bg-white p-[10rem] py-[7rem] shadow-[0_18px_30px_rgba(119,71,255,0.1)]'>
+        {/* ID, PW 입력란 */}
         <div className='flex w-full flex-col gap-10'>
           <Input
             aria-label='아이디'
@@ -22,6 +23,7 @@ export function LoginFormCard() {
             icon={<PwIcon />}
           />
         </div>
+        {/* 로그인 버튼 */}
         <Button
           size='xl'
           shape='circle'
