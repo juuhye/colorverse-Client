d1d01587f12ab4ca28c5fd1347a0a002ab287692
juuhye <165981565+juuhye@users.noreply.github.com>
2026-07-08 17:06:45 +0900

    build(supabase-setting): supabase 세팅

--- src/shared/ui/icon-toggle/IconToggle.tsx
+++ src/shared/ui/icon-toggle/IconToggle.tsx
@@ -1,5 +1,5 @@
 import { cva } from 'class-variance-authority'
-import { cn } from '@/shared/lib/utils'
+import { cn } from '@/shared/lib/utils/cn'
 import { IconToggleProps } from './type'
 import { useControllableState } from '@/shared/lib/hooks/useControllableState'
 
