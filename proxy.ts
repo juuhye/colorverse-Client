163e7d58418678557e56fe2aab72c70a24b1c1f8
juuhye <165981565+juuhye@users.noreply.github.com>
2026-07-14 17:41:22 +0900

    feat(login-page): login 가드 양방향 로직 수정

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,12 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
+const AUTH_PATHS = ['/login', '/login-form']
+
 export async function proxy(request: NextRequest) {
   let response = NextResponse.next({ request })
+  const { pathname } = request.nextUrl
+  const isAuthPage = AUTH_PATHS.some((path) => pathname.startsWith(path))
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -25,17 +29,20 @@ export async function proxy(request: NextRequest) {
     }
   )
 
-  // getUser()로 로그인 여부 확인 + /login, /login-form이면 리다이렉트
+  // getUser()로 로그인 여부 확인 + /login, /login-form, / 리다이렉트
   const {
     data: { user },
   } = await supabase.auth.getUser()
-  if (user) {
+
+  if (user && isAuthPage) {
     return NextResponse.redirect(new URL('/', request.url))
+  } else if (!user && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
   return response
 }
 
 // 미들웨어 실행 범위 지정
 export const config = {
-  matcher: ['/login', '/login-form'],
+  matcher: ['/', '/login', '/login-form'],
 }
