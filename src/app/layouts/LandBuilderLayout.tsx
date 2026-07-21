c7dbcdd1e8e665a5a71911a669a000afb5892771
juuhye <165981565+juuhye@users.noreply.github.com>
2026-07-21 17:31:07 +0900

    refactor(land-builder): coderabbit 리뷰 반영

--- src/app/layouts/LandBuilderLayout.tsx
+++ src/app/layouts/LandBuilderLayout.tsx
@@ -1,9 +1,8 @@
 import { HeaderBottom, HeaderTop } from '@/widgets/land-builder/header'
-import React from 'react'
 
 export function LandBuilderLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='h-dvh w-dvw'>
+    <div className='h-screen w-screen'>
       <header>
         <HeaderTop />
         <HeaderBottom />
--- src/widgets/land-builder/header/ui/HeaderBottom.tsx
+++ src/widgets/land-builder/header/ui/HeaderBottom.tsx
@@ -1,3 +1,4 @@
 export function HeaderBottom() {
+  // TODO: 추후 랜드 빌더 헤더 하단의 필터나 탭 메뉴가 추가될 자리입니다.
   return <div className='w-full'></div>
 }
