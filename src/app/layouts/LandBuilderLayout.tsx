571a3c82f1fb3db3676a5bb61f119ceb25b030b3
juuhye <165981565+juuhye@users.noreply.github.com>
2026-07-29 16:27:05 +0900

    refactor(land-builder): coderabbit 리뷰 수정

--- src/app/layouts/LandBuilderLayout.tsx
+++ src/app/layouts/LandBuilderLayout.tsx
@@ -11,14 +11,14 @@ export function LandBuilderLayout({ children }: { children: React.ReactNode }) {
       </header>
       <div className='w-full flex-1 overflow-hidden p-sm pt-0'>
         <div className='h-full w-full overflow-hidden rounded-[5rem]'>
+          {children}
           {/* 랜드 빌더 영역 - 임시 이미지*/}
           <Image
             src={MainMockImg}
-            alt='랜드 빌더 배경이미지'
+            alt=''
             className='h-full w-full object-cover'
           />
         </div>
-        {children}
       </div>
     </div>
   )
